42fca133fa101d8a6ddc5d236c7ca534f9982fe6
Moaath Alrajab <moaath@alrajab.net>
2022-10-03 19:29:37 -0400

    Initial commit

---
+++ app/src/main/java/edu/farmingdale/bcs421/bcs421_f22_w6_fragmentsdemo/Fragment01.kt
@@ -0,0 +1,13 @@
+package edu.farmingdale.bcs421.bcs421_f22_w6_fragmentsdemo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class Fragment01 : Fragment(R.layout.fragment_01) {
+
+
+}
