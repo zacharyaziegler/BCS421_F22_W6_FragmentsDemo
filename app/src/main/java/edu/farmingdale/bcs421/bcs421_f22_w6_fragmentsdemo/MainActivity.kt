4bb409a685a6c1e3ad6d3a17dc32045c6bd48415
Moaath Alrajab <moaath@alrajab.net>
2022-10-05 18:43:43 -0400

    Add sharedPref functions

--- app/src/main/java/edu/farmingdale/bcs421/bcs421_f22_w6_fragmentsdemo/MainActivity.kt
+++ app/src/main/java/edu/farmingdale/bcs421/bcs421_f22_w6_fragmentsdemo/MainActivity.kt
@@ -3,15 +3,28 @@ package edu.farmingdale.bcs421.bcs421_f22_w6_fragmentsdemo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
+
+
 class MainActivity : AppCompatActivity() {
+    val SHAREDPREF_FILENAME= "MYSHAREDPREF_FILE"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<TextView>(R.id.helloWorldTV).setOnClickListener{
+        findViewById<Button>(R.id.helloWorldBtn).setOnClickListener{
+            storeInSharedPref()
             startActivity(Intent(this, MainActivity2::class.java))
         }
+    }
+
+    private fun storeInSharedPref(){
+        var sharedpref= getSharedPreferences(SHAREDPREF_FILENAME, MODE_PRIVATE)
+        var editor=sharedpref.edit()
+        editor.putString("KEY", "This value is stored in shared pref")
+        editor.commit()
     }
 }
--- app/src/main/java/edu/farmingdale/bcs421/bcs421_f22_w6_fragmentsdemo/MainActivity2.kt
+++ app/src/main/java/edu/farmingdale/bcs421/bcs421_f22_w6_fragmentsdemo/MainActivity2.kt
@@ -3,10 +3,12 @@ package edu.farmingdale.bcs421.bcs421_f22_w6_fragmentsdemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
     lateinit var mBtn1:Button
     lateinit var mBtn2:Button
+    lateinit var mTV:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +16,7 @@ class MainActivity2 : AppCompatActivity() {
 
         mBtn1 = findViewById(R.id.btn1);
         mBtn2 = findViewById(R.id.btn2);
+        mTV = findViewById(R.id.tv1)
         var frgmnt01 = Fragment01()
         var frgmnt02 = Fragment02()
 
@@ -29,6 +32,12 @@ class MainActivity2 : AppCompatActivity() {
                 replace(R.id.framelayout1, frgmnt02)
                 commit()
             }
+            readFromSharedPref()
         }
+    }
+
+    private fun readFromSharedPref(){
+        var sharedPref= getSharedPreferences(MainActivity().SHAREDPREF_FILENAME, MODE_PRIVATE)
+        mTV.setText(sharedPref.getString("KEY", "not forund"))
     }
 }
